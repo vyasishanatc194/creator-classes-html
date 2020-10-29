a8b6abcfec945660247ec051440f6b4533a48759
Ishan Vyas <vyasishanatc194@gmail.com>
2020-10-29 18:44:16 +0530

    Working in creator details

--- assets/js/get-owl-slider.js
+++ assets/js/get-owl-slider.js
@@ -71,4 +71,62 @@ $(document).ready(function () {
 
     }
   });
+
+  $("#upcoming-live-streams-owl").owlCarousel({
+    loop: true,
+    nav: false,
+    dots: false,
+    stagePadding: 0,
+    margin: 20,
+    autoWidth:true,
+    autoplay: false,
+    smartSpeed: 2000,
+    responsive: {
+      0: {
+        items: 1,
+        autoWidth:false,
+        autoplay: true,
+      },
+      600: {
+        items: 1.9,
+        autoWidth:false,
+        autoplay: true,
+      },
+      1200: {
+        items: 1.2,
+      },
+      1600: {
+        items: 1.25,
+      }
+    }
+  });
+
+  $("#similar-creators-owl").owlCarousel({
+    loop: true,
+    nav: false,
+    dots: false,
+    stagePadding: 0,
+    margin: 15,
+    autoplay: false,
+    autoWidth:true,
+    smartSpeed: 2000,
+    responsive: {
+      0: {
+        items: 1,
+        autoWidth:false,
+        autoplay: true,
+      },
+      600: {
+        items: 2.3,
+        autoWidth:false,
+        autoplay: true,
+      },
+      1200: {
+        items: 3.1,
+      },
+      1600: {
+        items: 3.15,
+      }
+    }
+  });
 });
